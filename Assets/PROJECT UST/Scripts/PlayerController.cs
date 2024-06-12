02885a914c92dff81aa3aaa4f6db1d4ffa7b6080
Desktop-GiJae <liongijae@naver.com>
2024-06-12 13:49:09 +0900

    PlayerControllerScript Test

--- Assets/PROJECT UST/Scripts/PlayerController.cs
+++ Assets/PROJECT UST/Scripts/PlayerController.cs
@@ -6,16 +6,50 @@ namespace UST
 {
     public class PlayerController : MonoBehaviour
     {
+        public float moveSpeed = 5f;
+        public float rotateSpeed = 5f;
+        public float lastMouseX = 0;
+
+
         // Start is called before the first frame update
         void Start()
         {
-        
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            if(Input.GetKey(KeyCode.W))
+            {
+                transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += transform.forward * (-1) * moveSpeed * Time.deltaTime;
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                transform.position += transform.right * moveSpeed * Time.deltaTime;
+            }
+            if(Input.GetKey(KeyCode.A))
+            {
+                transform.position += transform.right * (-1) * moveSpeed * Time.deltaTime;
+            }
+
+            bool isMouseLeftMoved = lastMouseX - Input.mousePosition.x < 0;
+            bool isMouseMoved = Input.mousePosition.x != lastMouseX;
+            if(isMouseMoved)
+            {
+                transform.Rotate(Vector3.up * rotateSpeed * (isMouseLeftMoved ? -1 : 1) * Time.deltaTime);
+            }
+
+        }
+
+        private void LateUpdate()
+        {
+            lastMouseX = Input.mousePosition.x;
         }
     }
 }
